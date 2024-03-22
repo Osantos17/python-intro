e42d23798ebfea1cb7572c29d41c740389b33a81
Orlando Santos <Santos.Orlando17@gmail.com>
2024-03-21 23:33:47 -0700

    add intro-integers-floats

---
+++ intro-integers-floats.py
@@ -0,0 +1,17 @@
+# num = 10
+
+# num += 10
+
+# print(num)
+
+# print (round(3.75, 2))
+
+# num_1 = 3
+# num_2 = 2
+
+# print( num_1 != num_2)
+
+num_1 = '100'
+num_2 = '200'
+
+print(f'{int(num_1) + int(num_2)}')
